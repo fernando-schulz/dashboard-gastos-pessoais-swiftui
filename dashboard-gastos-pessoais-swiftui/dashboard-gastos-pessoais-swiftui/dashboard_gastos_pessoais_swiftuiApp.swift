260a97ce40512108fbc908910f3b6508135b2fdd
fernando-schulz <fernando.rsl3@gmail.com>
2025-04-21 10:26:51 -0300

    Commit inicial
    Commit inicial de criação de arquivos Swift

---
+++ dashboard-gastos-pessoais-swiftui/dashboard-gastos-pessoais-swiftui/dashboard_gastos_pessoais_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  dashboard_gastos_pessoais_swiftuiApp.swift
+//  dashboard-gastos-pessoais-swiftui
+//
+//  Created by Fernando Schulz on 21/04/25.
+//
+
+import SwiftUI
+
+@main
+struct dashboard_gastos_pessoais_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dashboard-gastos-pessoais-swiftui/dashboard-gastos-pessoais-swiftuiTests/dashboard_gastos_pessoais_swiftuiTests.swift
@@ -0,0 +1,16 @@
+//
+//  dashboard_gastos_pessoais_swiftuiTests.swift
+//  dashboard-gastos-pessoais-swiftuiTests
+//
+//  Created by Fernando Schulz on 21/04/25.
+//
+
+import Testing
+
+struct dashboard_gastos_pessoais_swiftuiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
